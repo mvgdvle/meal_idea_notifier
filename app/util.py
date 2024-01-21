a3d912bf804c3c492f0ad1a9630b2752312e79ec
moniquue <56364634+moniquue@users.noreply.github.com>
2024-01-21 20:34:17 +0100

    generate message content added

--- app/util.py
+++ app/util.py
@@ -11,3 +11,28 @@ def parse_config(config_file_name: str):
             raise e
 
         return config
+
+#zwracanie sformatowanej wiadomości
+def generate_message_content(dish_name, ingredients, instructions):
+
+# wzór wiadomości - nazwa dania + instrukcja + lista składników wraz z miarami 
+    meal_idea_template= """
+<b>{DISH_NAME}</b>
+
+<u>Cooking Instructions:</u>
+{COOKING_INSTRUCTIONS}
+
+<u>Ingredients:</u>
+{INGREDIENTS}
+"""
+    meal_idea_template = meal_idea_template.replace("{DISH_NAME}", dish_name).replace(
+        "{COOKING_INSTRUCTIONS}", instructions)
+    
+    cooking_ingredients = ""
+    for i, (ingredient, measure) in enumerate(ingredients.items()):
+        cooking_ingredients += f"{i+1}. {ingredient}: ({measure})\n"
+    
+    meal_idea_template = meal_idea_template.replace(
+        "{INGREDIENTS}", cooking_ingredients)
+    
+    return meal_idea_template
